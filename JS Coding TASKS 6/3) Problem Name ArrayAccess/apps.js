c8619cd019b19b50a5373aa61ac38b1253e1eb2b
unknown <you@example.com>
2021-06-06 21:42:11 +0500

    Added JS Coding TASKS 6 (Arrays)

--- JS Coding TASKS 6/3) Problem Name ArrayAccess/apps.js
+++ JS Coding TASKS 6/3) Problem Name ArrayAccess/apps.js
@@ -5,4 +5,4 @@ Print the first and last member of the same array
 names = ["Shairyar", "Alishah", "shahzaib", "Khan", "sehar", "Shireen"];
 let first = names[0];
     console.log(names[0]);
-    console.log(names[6]);
+    console.log(names[5]);
